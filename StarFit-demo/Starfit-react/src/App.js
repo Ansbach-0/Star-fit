a9339c6d1a561edb305e8d360c4087fbf76fc1ae
Andre Felipe Jorge Rachid <andre.rachid@cs.unicid.edu.br>
2025-10-11 19:53:28 -0300

    upgraded the react app to have a landing page and a login page. Added tailwindcss for styling.

---
+++ StarFit-demo/Starfit-react/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoginPage from '../starfitapp';
+
+function App() {
+  return <LoginPage />;
+}
+
+export default App;
---
+++ StarFit-demo/Starfit-react/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "../*.html",
+    "./*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
